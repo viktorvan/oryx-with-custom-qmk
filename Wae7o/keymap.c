714fe38734086696a3e0a78dcb5f345475825210
Viktor Andersson <v.andersson@me.com>
2024-11-30 00:02:04 +0100

    Allow chording for thumb keys

--- Wae7o/keymap.c
+++ Wae7o/keymap.c
@@ -171,6 +171,18 @@ void matrix_scan_user(void) {
   achordion_task();
 }
 
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Also allow same-hand holds when the other key is in the rows below the
+  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+  if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
 typedef struct {
     bool is_press_action;
     uint8_t step;
